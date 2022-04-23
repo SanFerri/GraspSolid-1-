82cc10de0964bf49d876660e035aa8b61fd872e1
SanFerri <santiago.ferraro@correo.ucu.edu.uy>
2022-04-23 15:50:59 -0300

    Comentarios extras

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -25,6 +25,22 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        /// <summary>
+        /// Agregue el método para calcular el coste total de producción en Recipe, esto lo
+        /// hice basándome en el patrón Expert. Ya que aquel que tiene los conocimientos necesarios
+        /// para poder llevar a cabo la función es Recipe, porque conoce todos los steps (Y, por ende
+        /// todos los costos unitarios, y el costo por hora del equipamiento).
+        /// </summary>
+        /// <returns></returns>
+        private double GetProductionCost()
+        {
+            double productionCost = 0;
+            foreach(Step step in this.steps)
+            {
+                productionCost += (step.Equipment.HourlyCost * (step.Time/3600)) + (step.Input.UnitCost * step.Quantity);
+            }
+            return productionCost;
+        }
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
@@ -33,6 +49,8 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+            // Aqui se imprime el costo obteniendo el double de la funcion GetProductionCost que creamos.
+            Console.WriteLine($"Costo: {this.GetProductionCost()}$");
         }
     }
 }
